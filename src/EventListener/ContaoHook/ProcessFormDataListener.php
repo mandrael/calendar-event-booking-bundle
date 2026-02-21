6184c4d3e96a7142e9452f172b97ccc00a3869f2
Marko Cupic <m.cupic@gmx.ch>
2026-02-21 14:36:56 +0100

    Log booking ID in ProcessFormDataListener
    Update the log message to include the booking ID and the event title by changing the sprintf format and arguments in ProcessFormDataListener. The previous message only included the event title and booking token; this change prepends the booking ID and reorders the placeholders. Review whether the booking token should still be included, as its argument was removed from this call.

--- src/EventListener/ContaoHook/ProcessFormDataListener.php
+++ src/EventListener/ContaoHook/ProcessFormDataListener.php
@@ -99,7 +99,7 @@ class ProcessFormDataListener
 
         $event = $bookingModuleInstance->getEvent();
 
-        $strText = \sprintf('New event booking for event "%s" and booking token %s.', $event->title, $booking->bookingToken);
+        $strText = \sprintf('New event booking ID %s for event "%s" and booking token %s.', $booking->id, $event->title);
 
         $this->contaoGeneralLogger?->info($strText);
     }
